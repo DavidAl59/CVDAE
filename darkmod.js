694bee03437eb5476a9efeef75440a619c43cc67
DavidAl59 <daengrand59@gmail.com>
2021-08-11 11:59:07 +0200

    cvmobilesection

--- darkmod.js
+++ darkmod.js
@@ -1,12 +1,13 @@
-function toggletheme() {
+console.log("hello") 
+function toggleTheme() {
     var theme = document.getElementById("light");
     // J'appelle ma variable par ID "light"
     // Je l'attribue à mon premier CSS "style2" SI l'user est sur le mode light
     // ou je le remplace à mon deuxième CSS "Styledark.css" si l'user est sur le mode Dark
     //SINON : Je reste en mode Light par défaut.
-    if (theme.getAttributer("href") == "./style2.css") {
+    if (theme.getAttribute("href") == "./style2.css") {
         theme.setAttribute("href","./Styledark.css");
     } else {
-        theme.setAttribute("href", "style2.css");
+        theme.setAttribute("href", "./style2.css");
     }
 }
